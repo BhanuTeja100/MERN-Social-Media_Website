9b651c75701855f24a54a2dee32031f84e5b2c3c
Bhanu_Teja <ombhanu100@gmail.com>
2023-09-24 02:14:53 +0530

    did changes

--- server/index.js
+++ server/index.js
@@ -29,11 +29,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors({
-  origin: ["https://mern-social-media-website-lup7p6086-ombhanu100-gmailcom.vercel.app/"],
-  methods: ["POST", "GET"],
-  credentials: true
-}));
+app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
